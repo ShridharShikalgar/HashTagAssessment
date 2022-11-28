01ad59b20455500c5969a17dd8f69c6da6317825
unknown <shreedhar281996@gmail.com>
2022-11-28 20:55:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+// those imports are important
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
